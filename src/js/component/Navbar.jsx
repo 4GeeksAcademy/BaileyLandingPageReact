2fa1f70ff5b3c5669b658197e38d7620515f11ce
Kathy Bailey Hines <135627670+klbailey@users.noreply.github.com>
2023-12-02 20:13:06 +0000

    Add css to navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const Navbar = () => {
     return (
-        <div className="w-full h-[80px] bg-black text-white ">
-            <a href="/" className="siteTitle max-w[1480px] m-auto w-full h-full flex justify-between items-center">Start Bootstrap</a>
-                <ul className="list-unstyled flex gap-4">
+        <div className="nav">
+            <a href="/" className="siteTitle">Start Bootstrap</a>
+                <ul>
                     <li>Home</li>
                     <li>About</li>
                     <li>Services</li>
